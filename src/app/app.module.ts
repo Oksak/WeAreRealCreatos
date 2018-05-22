73f3e348fe5a8ba88028a690ddf2fac6ec06fa87
Oksak <oneione90@gmail.com>
2018-05-22 16:05:51 +0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,23 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { MainHeaderComponent } from './main-header/main-header.component';
+import { PortfolioSectionComponent } from './portfolio-section/portfolio-section.component';
+import { LandingSectionComponent } from './landing-section/landing-section.component';
+import { FormSectionComponent } from './form-section/form-section.component';
+import { SectionBlogComponent } from './section-blog/section-blog.component';
+import { FooterComponent } from './footer/footer.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainHeaderComponent,
+    PortfolioSectionComponent,
+    LandingSectionComponent,
+    FormSectionComponent,
+    SectionBlogComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/section-blog/section-blog.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-section-blog',
+  templateUrl: './section-blog.component.html',
+  styleUrls: ['./section-blog.component.sass']
+})
+export class SectionBlogComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
